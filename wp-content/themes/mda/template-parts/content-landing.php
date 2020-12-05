8616511a27f9cb64f4057011e6b05faab4fd1453
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-12-05 11:58:08 +0100

    renommage certaines classes pour faciliser lecture

--- wp-content/themes/mda/template-parts/content-landing.php
+++ wp-content/themes/mda/template-parts/content-landing.php
@@ -15,48 +15,48 @@
 === HERO LANDING
 -->
 
-<section class="text-upper display--row display--between-x h-100 bg-gradient display--center-y display--overflow-hidden position--relative">
+<section class="position--relative size--h100 display--row display--between-x text--upper display--center-y display--overflow-hidden bg--gradient">
 
 	<!-- === Titre === -->
-	<div class="w-auto margin--m-none-children position--relative">
-		<h2 class="title--h4 color-grey position--relative">
+	<div class=" position--relative size--wauto margin--m-none-children">
+		<h2 class="position--relative title--h4 text--color-grey">
 			du 21 au 27 janvier 2021
 
 			<!-- === Separateur pointillé === -->
-			<span class="shape--elmt-border-dotted main position--xy_ b-50 l-20"></span>
+			<span class="position--xy_ b-50 l-20 shape--elmt-border-dotted shape--main"></span>
 		</h2>
 
-		<h1 class="title--title-landing text-letter-spacing color-main">
+		<h1 class="title--title-landing text--letter-spacing text--color-main">
 			festival de<br>
 			l'apprendre
 		</h1>
 
-		<h2 class="text-right title--h4 color-grey w-100">
+		<h2 class="size--w100 text--right title--h4 text--color-grey">
 			en famille, entre collègues, entre amis,<br />
 			venez fêter le plaisir d'apprendre
 		</h2>
 
-		<span class="shape--elmt-border-dotted main position--xy_ b-80 l-30 shape--rotate90"></span>
+		<span class="position--xy_ b-80 l-30 shape--elmt-border-dotted shape--main shape--rotate90"></span>
 	</div>
 
 	<!-- === Badges circulaires === --->
-	<div class="position--absolute w-40 h-70 position--xy_ t10 l40 display--between-x text-upper text-bold">
-		<div class="h-25 display--row end-y display--between-x">
-			<div class="shape--elmt-circle-b margin--m-b-auto bg-secondary color-white display--center">
+	<div class="position--absolute position--xy_ t10 l40 size--w40 size--h70  display--between-x text--upper text--bold">
+		<div class="size--h25 display--row display--end-y display--between-x">
+			<div class="margin--m-b-auto display--center shape--elmt-circle-b text--color-white bg--secondary">
 				<span>Découvrir</span>
 			</div>
-			<div class="shape--elmt-circle-m bg-warm color-white display--center">
-				<span class=" text-small">Rencontrer</span>
+			<div class="shape--elmt-circle-m display--center text--color-white bg--warm">
+				<span class="text--small">Rencontrer</span>
 			</div>
 		</div>
 
-		<div class="shape--elmt-circle-m bg-main color-white margin--m-l-s display--center">
-			<span class=" text-small">Expérimenter</span>
+		<div class="margin--m-l-s shape--elmt-circle-m display--center text--color-white bg--main">
+			<span class="text--small">Expérimenter</span>
 		</div>
 	</div>
 
 	<!-- === Illustration === --->
-	<figure class="h-100 bg-img_ bgauto bgbr"></figure>
+	<figure class="size--h100 bg--img_ bgauto bgbr"></figure>
 </section>
 
 
@@ -64,22 +64,22 @@
 === CONTENT
 -->
 
-<section class="display--row display--between-x wrap">
+<section class="display--row display--between-x display--wrap">
 
 	<!-- === Titre de la section === --->
-	<header class="block-title">
+	<header class="text-componant--block-title">
 		<h2 class="position--relative">
 			Découvrir<br />
 			le festival
 
-			<span class="shape--elmt-border-dotted main position--xy_ t-10 l-60"></span>
+			<span class="position--xy_ t-10 l-60 shape--elmt-border-dotted shape--main"></span>
 		</h2>
 
 		<h3>Une journée pour apprendre et découvrir</h3>
 	</header>
 
 	<!-- === description de la section === --->
-	<div class="block-parag">
+	<div class="text-componant--block-parag">
 		<p>
 			Première édition sur le territoire lyonnais, cette manifestation est impulsée par La Maison de l'Apprendre dont le rôle 
 			est de fédérer un réseau territorial d'acteurs pour répondre, ensemble, aux enjeux actuels et futurs d'apprentissage et 
@@ -89,18 +89,18 @@
 </section>
 
 <section class="display--row display--between-x wrap">
-	<header class="block-title">
+	<header class="text-componant--block-title">
 		<h2 class="position--relative">
 			Ateliers<br />
 			et conférences
 
-			<span class="shape--elmt-border-dotted main position--xy_ t-10 l-60"></span>
+			<span class="position--xy_ t-10 l-60 shape--elmt-border-dotted shape--main"></span>
 		</h2>
 
 		<h3>Comprendre le monde de demain</h3>
 	</header>
 
-	<div class="block-parag">
+	<div class="text-componant--block-parag">
 		<p>
 			Première édition sur le territoire lyonnais, cette manifestation est impulsée par La Maison de l'Apprendre dont le rôle 
 			est de fédérer un réseau territorial d'acteurs pour répondre, ensemble, aux enjeux actuels et futurs d'apprentissage et 
@@ -114,18 +114,18 @@
 === SELECTION DES ATELIERS
 -->
 
-<section class="bg-gradient">
-	<div class="bg-white w-100">
+<section class="bg--gradient">
+	<div class="size--w100 bg--white">
 
 		<!-- === filter bar === --->
-		<header class="bg-white-pure h-10 display--center-x">
-			<form class="display--row w-100 display--center-y display--between-y">
+		<header class="size--h10 display--center-x bg--white-pure">
+			<form class=" size--w100 display--row display--center-y display--between-y">
 
 				<!-- === search bar === --->
-				<input type="text" class="searchbar w-50 border" name="search" placeholder="Rechercher un atelier">
+				<input type="text" class="size--w50" name="search" placeholder="Rechercher un atelier">
 
 				<!-- === filter on tag === --->
-				<div class="display--row align--center margin--m-s-children border">
+				<div class="margin--m-s-children display--row display--center">
 					<input type="checkbox" name="all" id="all">
 					<label for="all">Tout public</label>
 
@@ -137,7 +137,7 @@
 				</div>
 
 				<!-- === change display : cards by filter (date, category, etc...) / cards by workshop / png programm === --->
-				<ul class="display--row display--center-y display--between-x margin--m-s-children shape--border-l-children shape--border-grey-children">
+				<ul class="margin--m-s-children display--row display--center-y display--between-x shape--border-l-children shape--border-grey-children">
 					<li>Tous</li>	
 					<li>Filtrer</li>
 					<li>Programme</li>
@@ -146,18 +146,18 @@
 		</header>
 
 		<!-- === Articles === --->
-		<div class="card-gallerie card-gallerie-4 p-m ">
-			<article class="card bg-white-pure display--overflow-hidden">
-				<header class="head card-bg h-20"></header>
-				<div class="body">
-					<ul class="card-tag">
+		<div class="card--gallerie card--gallerie-4 padding--m">
+			<article class="card--card display--overflow-hidden padding--s-children bg--white-pure">
+				<header class="size--h20 card--bg structure--head"></header>
+				<div class="structure--body">
+					<ul class="card--tag">
 						<li>24/11/2020</li>
 						<li>14h30</li>
 						<li class="margin--m-l-auto">jeunesse</li>
 					</ul>
 
-					<h3 class="position--relative w-100 title--h2 margin--m-b-s">
-						<span class="shape--elmt-border-dotted main position--xy_ t-50 r-30"></span>
+					<h3 class="position--relative size--w100 margin--m-b-s title--h2">
+						<span class="position--xy_ t-50 r-30 shape--elmt-border-dotted shape--main"></span>
 						Titre atelier
 					</h3>	
 
@@ -165,22 +165,22 @@
 						Courte description lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora earum numquam libero quod eum
 					</p>
 				</div>
-				<footer class="foot end-y">
-					<button class="btn">Voir l'atelier</button>
+				<footer class="structure--foot display--end-y">
+					<button class="button--btn">Voir l'atelier</button>
 				</footer>
 			</article>
 
-			<article class="card bg-white-pure display--overflow-hidden">
-				<header class="head card-bg h-20"></header>
-				<div class="body">
-					<ul class="card-tag">
+			<article class="card--card display--overflow-hidden padding--s-children bg--white-pure">
+				<header class="size--h20 card--bg structure--head"></header>
+				<div class="structure--body">
+					<ul class="card--tag">
 						<li>24/11/2020</li>
 						<li>14h30</li>
 						<li class="margin--m-l-auto">jeunesse</li>
 					</ul>
 
-					<h3 class="position--relative w-100 title--h2 margin--m-b-s">
-						<span class="shape--elmt-border-dotted main position--xy_ t-50 r-30"></span>
+					<h3 class="position--relative size--w100 margin--m-b-s title--h2">
+						<span class="position--xy_ t-50 r-30 shape--elmt-border-dotted shape--main"></span>
 						Titre atelier
 					</h3>	
 
@@ -188,22 +188,22 @@
 						Courte description lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora earum numquam libero quod eum
 					</p>
 				</div>
-				<footer class="foot end-y">
-					<button class="btn">Voir l'atelier</button>
+				<footer class="structure--foot display--end-y">
+					<button class="button--btn">Voir l'atelier</button>
 				</footer>
 			</article>
 
-			<article class="card bg-white-pure display--overflow-hidden">
-				<header class="head card-bg h-20"></header>
-				<div class="body">
-					<ul class="card-tag">
+			<article class="card--card display--overflow-hidden padding--s-children bg--white-pure">
+				<header class="size--h20 card--bg structure--head"></header>
+				<div class="structure--body">
+					<ul class="card--tag">
 						<li>24/11/2020</li>
 						<li>14h30</li>
 						<li class="margin--m-l-auto">jeunesse</li>
 					</ul>
 
-					<h3 class="position--position--relative w-100 title--h2 margin--m-b-s">
-						<span class="shape--elmt-border-dotted main position--xy_ t-50 r-30"></span>
+					<h3 class="position--relative size--w100 margin--m-b-s title--h2">
+						<span class="position--xy_ t-50 r-30 shape--elmt-border-dotted shape--main"></span>
 						Titre atelier
 					</h3>	
 
@@ -211,22 +211,22 @@
 						Courte description lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora earum numquam libero quod eum
 					</p>
 				</div>
-				<footer class="foot end-y">
-					<button class="btn">Voir l'atelier</button>
+				<footer class="structure--foot display--end-y">
+					<button class="button--btn">Voir l'atelier</button>
 				</footer>
 			</article>
 
-			<article class="card bg-white-pure display--overflow-hidden">
-				<header class="head card-bg h-20"></header>
-				<div class="body">
-					<ul class="card-tag">
+			<article class="card--card display--overflow-hidden padding--s-children bg--white-pure">
+				<header class="size--h20 card--bg structure--head"></header>
+				<div class="structure--body">
+					<ul class="card--tag">
 						<li>24/11/2020</li>
 						<li>14h30</li>
 						<li class="margin--m-l-auto">jeunesse</li>
 					</ul>
 
-					<h3 class="position--relative w-100 title--h2 margin--m-b-s">
-						<span class="shape--elmt-border-dotted main position--xy_ t-50 r-30"></span>
+					<h3 class="position--relative size--w100 margin--m-b-s title--h2">
+						<span class="position--xy_ t-50 r-30 shape--elmt-border-dotted shape--main"></span>
 						Titre atelier
 					</h3>	
 
@@ -234,16 +234,16 @@
 						Courte description lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora earum numquam libero quod eum
 					</p>
 				</div>
-				<footer class="foot end-y">
-					<button class="btn">Voir l'atelier</button>
+				<footer class="structure--foot display--end-y">
+					<button class="button--btn">Voir l'atelier</button>
 				</footer>
-			</article>	
+			</article>
 		</div>
 	</div>
 </section>
 
-<footer class="bg-gradient h-20 ">
-	<figure class="bg-footer w-100 h-100p p-xb">
+<footer class="size--h20 bg--gradient">
+	<figure class="size--w100 size--h100p padding--xb bg--footer">
 	contact
 	Twitter
 	Réseau
