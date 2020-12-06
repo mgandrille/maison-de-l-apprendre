3487ea51346a0c1853852a24c6f9b7204ce10d4a
Marie Gandrille <mgandrille@yahoo.fr>
2020-12-06 15:31:59 +0100

    Création de la fonction permettant d'avoir accès à l'api de HelloAsso (et au token access)

--- wp-content/themes/mda/inc/hello-asso.php
+++ wp-content/themes/mda/inc/hello-asso.php
@@ -1,38 +1,59 @@
 <?php
 /**
- * HELLO ASSO association
+ * HELLO ASSO API
  *
  * @package mda
  */
 
-// Informations about the application
-$urlApi = "https://api.helloasso.com/oauth2/token";
+// Informations about mda
 $clientKey = "";
 $clientSecret = "";
 
-$data = array(
-    'grant_type' => 'client_credentials',
-    'client_id' => $clientKey,
-    'client_secret' => $clientSecret
-);
-$dataFields = http_build_query($data);
+
+/**
+ * access_api
+ * Permits to have authentified access to HelloAsso API
+ *
+ * @return array $accessToken and $refreshToken
+ *
+ */
+function access_api() {
+    global $clientKey, $clientSecret;
+    $urlApi = "https://api.helloasso.com/oauth2/token";
+
+    $data = array(
+        'grant_type' => 'client_credentials',
+        'client_id' => $clientKey,
+        'client_secret' => $clientSecret
+    );
+    $dataFields = http_build_query($data);
 
 
-// CURL CALL
-$ch = curl_init();
+    // CURL CALL
+    $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_URL, $urlApi);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('application/x-www-form-urlencoded'));
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $dataFields);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_URL, $urlApi);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('application/x-www-form-urlencoded'));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataFields);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$result = curl_exec($ch);
-// d($result);
+    $result = curl_exec($ch);
+    $result = json_decode($result);
 
-// d(curl_getinfo($ch));
-// d(curl_errno($ch));
-// d(curl_error($ch));
+    // Get Token Access
+    $accessToken = $result->access_token;
+    $refreshToken = $result->refresh_token;
 
-curl_close($ch);
+    // d(curl_getinfo($ch));
+    // d(curl_errno($ch));
+    // d(curl_error($ch));
+
+    curl_close($ch);
+
+    return [$accessToken, $refreshToken];
+
+}
+
+
