d9677b6b417c85e81845ab8a99477bac5d15277e
Marie Gandrille <mgandrille@yahoo.fr>
2020-12-10 13:48:29 +0100

    Modification du CURL

--- wp-content/themes/mda/inc/hello-asso.php
+++ wp-content/themes/mda/inc/hello-asso.php
@@ -8,96 +8,195 @@
 // Informations about mda
 $clientKey = "";
 $clientSecret = "";
+$url = "https://api.helloasso.com/oauth2/token";
+$authorizeUrl = 'https://auth.helloasso.com/authorize';
+$siteUrl = $_SERVER['HTTP_REFERER'];
+$pkce = '';
+
+//Tell cURL where our certificate bundle is located.
+$certificate = "C:\MAMP\cacert.pem";
+
+/**
+ * @return array|string
+ */
+function firstTokens()
+{
+    global $clientKey, $clientSecret, $url;
+
+    $post = [
+        'client_id'     => $clientKey,
+        'client_secret' => $clientSecret,
+        'grant_type'    => 'client_credentials'
+    ];
+
+    $tokens = postCurl($url, $post);
+
+    $refreshToken = $tokens['refresh_token'];
+
+    return getSecondTokens($refreshToken);
+}
 
 
 /**
- * access_api
- * Permits to have authentified access to HelloAsso API
- *
- * @return array $accessToken and $refreshToken
- *
+ * @param string $refreshToken
+ * @return array|string
  */
-function access_api() {
-    global $clientKey, $clientSecret;
-    $urlApi = "https://api.helloasso.com/oauth2/token";
+function getSecondTokens(string $refreshToken)
+{
+    global $clientKey, $url;
 
-    $data = array(
-        'grant_type' => 'client_credentials',
-        'client_id' => $clientKey,
-        'client_secret' => $clientSecret
-    );
-    $dataFields = http_build_query($data);
+    $post = [
+        'client_id'     => $clientKey,
+        'grant_type'    => 'refresh_token',
+        'refresh_token' => $refreshToken
+    ];
+
+    $tokens = postCurl($url, $post);
+
+    return $tokens;
+}
+
+// d(firstTokens());
+
+function getAuthRequest()
+{
+    global $clientKey, $authorizeUrl, $siteUrl;
+
+    $pkce = getPKCE();
+
+    $url = $authorizeUrl. '?client_id=' .$clientKey. '&redirect_uri='.$siteUrl.'&code_challenge=' .$pkce. '&code_challenge_method=S256';
+
+    return $url;
+}
+// d(getAuthRequest());
 
 
-    // CURL CALL
-    $ch = curl_init();
+/**
+ * @return string
+ */
+function getPKCE()
+{
+    global $pkce;
 
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_URL, $urlApi);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('application/x-www-form-urlencoded'));
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataFields);
+    if(strlen($pkce) <= 43){
+
+        $authString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+
+        $randString = '';
+
+        for ($i = 0; $i < rand(43, 128); $i++) {
+            $randString .= $authString[rand(0, strlen($authString) - 1)];
+        }
+
+        $pkceCode = $randString;
+
+        $sha = hash('sha256', $pkceCode);
+        $base = base64_encode($sha);
+        $urlencode = urlencode($base);
+
+        $pkce = $urlencode;
+    }
+
+    return $pkce;
+
+}
+// d(getPKCE());
+
+/**
+* @param array $post
+* @return array|string
+*/
+function postCurl($url, array $post)
+{
+    global $certificate;
+
+    $array = http_build_query($post);
+
+    $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $array);
+    curl_setopt($ch, CURLOPT_CAINFO, $certificate);
+    curl_setopt($ch, CURLOPT_CAPATH, $certificate);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('application/x-www-form-urlencoded'));
+    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-    $result = curl_exec($ch);
-    $result = json_decode($result);
-    // d($result);
-
-    // Get Token Access
-    $accessToken = $result->access_token;
-    $refreshToken = $result->refresh_token;
-
-    // d(curl_getinfo($ch));
-    // d(curl_errno($ch));
-    // d(curl_error($ch));
-
+    $response = curl_exec($ch);
+    $error = curl_error($ch);
     curl_close($ch);
 
-    return [$accessToken, $refreshToken];
+    return checkCurlError($response, $error);
+}
+
+/**
+ * @param $url
+ * @param $headers
+ * @return array|string
+ */
+function getCurl($url, $headers)
+{
+    global $certificate;
+    $request = "{$url}";
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $request,
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_CONNECTTIMEOUT => 120,
+        CURLOPT_TIMEOUT => 120,
+        CURLOPT_RETURNTRANSFER => 1,
+        // CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_CAINFO => $certificate,
+        CURLOPT_CAPATH => $certificate,
+
+    ));
+
+    $response = curl_exec($curl);
+    $error = curl_error($curl);
+    curl_close($curl);
+
+
+    return checkCurlError($response, $error);
 }
 
 
-function refresh_token() {
-    global $clientKey;
-    $urlApi = "https://api.helloasso.com/oauth2/token";
-    $refreshToken = access_api()[1];
+/**
+ * @param $response
+ * @param $error
+ * @return array|string
+ */
+function checkCurlError($response, $error)
+{
+    $decodeResponse = json_decode($response, true);
 
-    $data = array(
-        'grant_type' => 'refresh_token',
-        'client_id' => $clientKey,
-        'refresh_token' => $refreshToken
-    );
-    $dataFields = http_build_query($data);
+    if(!$response || is_null($decodeResponse)){
 
+        if(isset($response['error'])){
+            $error = $response['error'];
+        }else if(empty($error)){
+            $error = 'Unknown error with curl';
+        }
 
-    // CURL CALL
-    $ch = curl_init();
+        return [ 'error' => $error ];
 
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_URL, $urlApi);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('application/x-www-form-urlencoded'));
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataFields);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    }else{
 
-    $result = curl_exec($ch);
-    $result = json_decode($result);
-
-    return $result;
-
+        return $decodeResponse;
+    }
 }
 
-$refreshToken = refresh_token();
 
-// d(access_api(), $refreshToken);
-
-
+/**
+ * Get all informations of all forms of the API Hello Asso for MDA
+ */
 function get_forms_infos() {
     $url = "https://api.helloasso.com/v5/organizations/la-maison-de-l-apprendre/forms?states=Public&formTypes=Event&pageIndex=1&pageSize=30";
     $request = "{$url}";
+    $token = firstTokens();
+
     $headers = [
         'Accepts: application/json',
-        'authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMjk1YzhkN2M4ZjA0NDE2YWY4NWFkNjA0Nzk0MDc5YiIsImNwcyI6WyJBY2Nlc3NQdWJsaWNEYXRhIiwiQWNjZXNzVHJhbnNhY3Rpb25zIl0sInVycyI6Ik9yZ2FuaXphdGlvbkFkbWluIiwibmJmIjoxNjA3Mzc4ODIzLCJleHAiOjE2MDczODA2MjMsImlzcyI6Imh0dHBzOi8vYXBpLmhlbGxvYXNzby5jb20iLCJhdWQiOiJkMTgwZWI2MmE2MmI0NmM0OTMxYTYzNzY2NWM3NGQ4MiJ9.Ieu_AjsB8RA4g-sDg3UCBZV_5Gf5qBYnXfXnZyHgEwtWDjlKv91jv39v0C_3QRjT9NaUFqJLTVh04phbqgGAb_LQWWxzQP0HQWLNm6avo-_07TKJqVKcndUzT_uJWJ1X77eXivXVXYhW6_Ekhx-Gdr3OZQ8lPxwUo5mHlnpeOhXRKoVRps5OQAidP7Pnyf6YT8-COLJZctbf4glAqgAcZ8Il_I8k1cYHH7hcFOPLF5j6_u6uCTyZCSFCxkqUajg9_FazAutzqCARc6nZ0bt_UMWIbj8wfqrwj7YQ6bFxNoX9U-8yYHO1UzG0o7pqwPKlNdiiXpeuc7a7_fUhu0Ng9A',
+        'Authorization: Bearer '.$token,
     ];
 
     $curl = curl_init();
@@ -131,16 +230,9 @@ function get_articles() {
         foreach($events as $event) {
             // d(new DateTime($event->endDate));
             // d($event, $event->endDate);
+            $eventInfo = get_object_vars($event);
             if(new DateTime($event->endDate) >= $today && $event->formSlug !== 'dejeuners-communaute-4')
             {
-                $eventInfo['post_title'] = $event->title;
-                $eventInfo['post_content'] = $event->description;
-                $eventInfo['startDate'] = $event->startDate;
-                $eventInfo['endDate'] = $event->endDate;
-                $eventInfo['widgetButtonUrl'] = $event->widgetButtonUrl;
-                $eventInfo['state'] = $event->state;
-                $eventInfo['post_status'] = 'publish';
-
                 array_push($eventDatas, $eventInfo);
             }
         }
@@ -149,4 +241,4 @@ function get_articles() {
     // d($eventDatas);
     return $eventDatas;
 }
-
+// d(get_articles());
