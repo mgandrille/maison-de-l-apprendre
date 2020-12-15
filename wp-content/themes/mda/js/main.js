f28aa33d71e539deff916c9fc7b48bd826d8c4d1
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-12-15 20:27:04 +0100

    search bar + checkbox combinés

--- wp-content/themes/mda/js/main.js
+++ wp-content/themes/mda/js/main.js
@@ -4,14 +4,14 @@
  * Handles toggling the navigation menu for small screens and enables TAB key
  * navigation support for dropdown menus.
  */
-( function() {
-	const navMenu = document.querySelector( '.nav--bar' );
+/*( function() {
+	const navMenu = document.querySelector( '.nav--menu' );
 	const navMenuBtn = document.getElementById( 'nav-menu-btn' );
 
 	navMenuBtn.addEventListener( 'click', () => {
 		navMenu.classList.toggle( 'display--flex' );
 	} );
-}() );
+}() );*/
 
 	
 /**
@@ -19,12 +19,44 @@
  */
 
 const form = document.getElementById('article-filter');
+const searchbar = document.getElementById('searchbar');
+
+let searchRegex = '';
 let storedFilterNames = [];
 
 const isotope = new Isotope( '.grid', {
 	itemSelector: '.grid-item',
 	layoutMode: 'fitRows',
+	filter: (e) => {
+		let searchResult = searchRegex ? e.textContent.match(searchRegex) : true;
+		let btnResult = storedFilterNames.length ? e.matches( storedFilterNames.join('') ) : true;
+
+		return searchResult && btnResult;
+	}
 });
+
+/**
+ * Empêche la searchbar de tourner en boucle
+ * @param {*} fn 
+ * @param {*} threshold 
+ */
+function debounce( fn, threshold ) {
+	let timeout;
+
+	threshold = threshold || 100;
+
+	return function debounced() {
+		clearTimeout(timeout);
+		const args = arguments;
+		const _this = this;
+
+		function delayed() {
+			fn.apply( _this, args );
+		}
+
+		timeout = setTimeout( delayed, threshold );
+	};
+}
 
 form.addEventListener('change', function(event) {
 	const checkbox = event.target;
@@ -34,13 +66,17 @@ form.addEventListener('change', function(event) {
 		checkbox.classList.add('is-checked');
 		storedFilterNames.push(filterName);
 
-		return isotope.arrange({ filter: storedFilterNames.join('') });
+		return isotope.arrange();
 	}
 	
 	checkbox.classList.remove("is-checked");
-
-	// On retire uniquement le filtre décoché du tableaux des filtres
 	storedFilterNames = storedFilterNames.filter(filter => !filter.includes(filterName))
 
-	return isotope.arrange({ filter: storedFilterNames.join('') });
+	return isotope.arrange();
 })
+
+searchbar.addEventListener( 'keyup', debounce( function(e) {
+	searchRegex = new RegExp( e.target.value, 'gi' );
+	isotope.arrange();
+	
+}, 200 ) );
