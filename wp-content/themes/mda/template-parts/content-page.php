ecd66d8036103df399dae3fa94e592b49d578bb9
Marie Gandrille <mgandrille@yahoo.fr>
2020-12-15 23:04:23 +0100

    Finalisation de la mise en page des pages "lambda"

--- wp-content/themes/mda/template-parts/content-page.php
+++ wp-content/themes/mda/template-parts/content-page.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article class="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article class="article section" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="header entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
@@ -29,26 +29,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="footer entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'mda' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
